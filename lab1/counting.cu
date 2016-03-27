ee6d758ac5eaf7e8a8a2dc534e3f4d1418f4a9cb
Mu Yang <emfomy@gmail.com>
2016-03-27 19:29:22 +0800

    [lab1] Update 'CountPosition' and 'Part3'.

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -7,7 +7,10 @@
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
 
+const int kMaxWordSize = 512;
 const int kNumThread = 1024;
+
+static_assert(kMaxWordSize <= kNumThread, "kMaxWordSize must be less than kNumThread.");
 
 __device__ __host__ int CeilDiv( int a, int b ) { return (a-1)/b + 1; }
 __device__ __host__ int CeilAlign( int a, int b ) { return CeilDiv(a, b) * b; }
@@ -17,25 +20,28 @@ __device__ __host__ int CeilAlign( int a, int b ) { return CeilDiv(a, b) * b; }
 ///
 __global__
 void CountPositionDevice1( const char *text, int *pos, int text_size ) {
-  auto idx = threadIdx.x;
-  auto idx_shift = blockIdx.x * blockDim.x;
-  text += idx_shift;
-  pos  += idx_shift;
+  auto idx = blockIdx.x * blockDim.x + threadIdx.x;
+  auto idx_end = (blockIdx.x + 1) * blockDim.x;
+  if ( idx_end > text_size ) {
+    idx_end = text_size;
+  }
 
   // Initialize position
-  if ( text[idx] == '\n' ) {
-    pos[idx] = 0;
-  } else if ( idx == 0 && idx_shift == 0 ) {
-    pos[idx] = 1;
-  } else {
-    pos[idx] = -1;
+  if ( idx < idx_end ) {
+    if ( text[idx] == '\n' ) {
+      pos[idx] = 0;
+    } else if ( idx == 0 ) {
+      pos[idx] = 1;
+    } else {
+      pos[idx] = -1;
+    }
   }
   __syncthreads();
 
   // Binary search
-  for ( auto k = 1; k < blockDim.x; k *= 2 ) {
+  for ( auto k = 1; k <= kMaxWordSize; k *= 2 ) {
     auto idxk = idx + k;
-    if ( idxk < blockDim.x && idxk+idx_shift < text_size && pos[idx] >= 0 && pos[idxk] < 0 ) {
+    if ( idxk < idx_end && pos[idx] >= 0 && pos[idxk] < 0 ) {
       pos[idxk] = pos[idx] + k;
     }
     __syncthreads();
@@ -91,14 +97,14 @@ int ExtractHead( const int *pos, int *head, int text_size) {
 ///
 __global__
 void Part3Device( char *text, int *pos, int *head, int text_size, int n_head ) {
-  auto i_word = blockIdx.x * blockDim.x + threadIdx.x;
-  int idx_end = (i_word == n_head-1) ? text_size : head[i_word+1];
+  auto idx_start = head[blockIdx.x];
+  auto idx_end = ((blockIdx.x == n_head-1) ? text_size : head[blockIdx.x+1]);
   while ( pos[--idx_end] == 0 );
 
   char temp;
-  for ( auto i = 0; i < (idx_end-head[i_word]) / 2; ++i ) {
-    temp = text[head[i_word]+i];
-    text[head[i_word]+i] = text[idx_end-i];
+  for ( auto i = threadIdx.x; i < (idx_end-idx_start) / 2; i += blockDim.x ) {
+    temp = text[idx_start+i];
+    text[idx_start+i] = text[idx_end-i];
     text[idx_end-i] = temp;
   }
 }
@@ -121,7 +127,7 @@ void Part3( char *text, int *pos, int *head, int text_size, int n_head ) {
 //   }
 //   printf("\n");
 
-  Part3Device<<<n_head / kNumThread + 1, kNumThread>>>(text, pos, head, text_size, n_head);
+  Part3Device<<<n_head, kNumThread>>>(text, pos, head, text_size, n_head);
 
 // #pragma warning
 //   cudaMemcpy(text_host, text, sizeof(char) * kCheckSize, cudaMemcpyDeviceToHost);
