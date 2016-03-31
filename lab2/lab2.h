99b0b058aa0aab4ad441b9ffe8ceda967f6ce957
Mu Yang <emfomy@gmail.com>
2016-03-31 12:31:57 +0800

    [lab2] Add Makefile.

--- lab2/lab2.h
+++ lab2/lab2.h
@@ -4,16 +4,16 @@
 using std::unique_ptr;
 
 struct Lab2VideoInfo {
-	unsigned w, h, n_frame;
-	unsigned fps_n, fps_d;
+  unsigned w, h, n_frame;
+  unsigned fps_n, fps_d;
 };
 
 class Lab2VideoGenerator {
-	struct Impl;
-	unique_ptr<Impl> impl;
-public:
-	Lab2VideoGenerator();
-	~Lab2VideoGenerator();
-	void get_info(Lab2VideoInfo &info);
-	void Generate(uint8_t *yuv);
+  struct Impl;
+  unique_ptr<Impl> impl;
+ public:
+  Lab2VideoGenerator();
+  ~Lab2VideoGenerator();
+  void get_info(Lab2VideoInfo &info);
+  void Generate(uint8_t *yuv);
 };
