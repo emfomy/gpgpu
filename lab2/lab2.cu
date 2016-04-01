d32fc7cf2990ae996da5c5e8aaa3a398a7ede975
Mu Yang <emfomy@gmail.com>
2016-04-01 21:09:34 +0800

    [lab2] Use multiple point per pixel.

--- lab2/lab2.cu
+++ lab2/lab2.cu
@@ -1,74 +1,116 @@
 #include "lab2.h"
-static const unsigned W = 1280;
-static const unsigned H = 960;
-static const unsigned NFRAME = 1440;
-static const int kMaxIter = 256;
-// static const double kCenterX = -0.1528;
-// static const double kCenterY =  1.0397;
-static const double kCenterX = -0.6367543465823900;
-static const double kCenterY =  0.6850312970836773;
+static const unsigned kW = 1280;
+static const unsigned kH = 960;
+static const unsigned kNFrame = 720;
+static const int kPixelDim = 2;
+static const unsigned kFullW = kW*kPixelDim;
+static const unsigned kFullH = kH*kPixelDim;
+static const int kMaxIter = 1024;
+static const double kZoomBase = -0.693147180559945309417232121458/24;
+static const double kCenterX = -0.77568377;
+static const double kCenterY =  0.13646737;
+// static const double kCenterX = -0.6367543465823900;
+// static const double kCenterY =  0.6850312970836773;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///
-///
+__device__ void QuadraticMap( double zx, double zy, double &fx, double &fy, double cx, double cy ) {
+    fx = (zx*zx - zy*zy) + cx;
+    fy = 2*zx*zy + cy;
+}
+
+__device__ void Iter2YUV( int iter, uint8_t &y, uint8_t &u, uint8_t &v ) {
+  double du, dv;
+  if ( iter == kMaxIter ) {
+    y = 0;
+    u = 128;
+    v = 128;
+  } else {
+    sincos(double(iter/8.0), &du, &dv);
+    y = 128;
+    u = -du*127 + 128;
+    v =  dv*127 + 128;
+  }
+}
+
 __global__
-void MandelbrotSet( uint8_t Y[][W], uint8_t U[][W/2], uint8_t V[][W/2], int frame ) {
-  int x = blockIdx.x * blockDim.x + threadIdx.x;
-  int y = blockIdx.y * blockDim.y + threadIdx.y;
+void MandelbrotSet( uint8_t (*colorY)[kW], uint8_t (*colorU)[kW/2], uint8_t (*colorV)[kW/2],
+                    uint8_t (*colorFullY)[kFullW], uint8_t (*colorFullU)[kFullW], uint8_t (*colorFullV)[kFullW], int frame ) {
+  int idxx = blockIdx.x * blockDim.x + threadIdx.x;
+  int idxy = blockIdx.y * blockDim.y + threadIdx.y;
   int dimx = gridDim.x * blockDim.x;
   int dimy = gridDim.y * blockDim.y;
-  double zoom = pow(1.0 + 1.0/24.0, double(frame));
-  double cx = double(x-dimx/2) / 64.0 / zoom + kCenterX,
-         cy = double(y-dimy/2) / 64.0 / zoom + kCenterY,
-         zx = 0, zy = 0, wx, wy;
+  double zoom = exp(frame * kZoomBase);
+
+  // Compute iteration number
+  double cx = double(idxx-dimx/2) / (64.0*kPixelDim) * zoom + kCenterX,
+         cy = double(idxy-dimy/2) / (64.0*kPixelDim) * zoom + kCenterY,
+         zx = 0, zy = 0, fx, fy;
   int iter;
   for ( iter = 0; (zx*zx + zy*zy) < 4.0 && iter < kMaxIter; ++iter ) {
-    wx = (zx*zx - zy*zy) + cx;
-    wy = 2*zx*zy + cy;
-    zx = wx; zy = wy;
+    QuadraticMap(zx, zy, fx, fy, cx, cy);
+    zx = fx; zy = fy;
   }
-  if ( iter == kMaxIter ) {
-    Y[2*y  ][2*x  ] = 0;
-    Y[2*y  ][2*x+1] = 0;
-    Y[2*y+1][2*x  ] = 0;
-    Y[2*y+1][2*x+1] = 0;
-    U[y][x] = 128;
-    V[y][x] = 128;
-  } else {
-    Y[2*y  ][2*x  ] = 128;
-    Y[2*y  ][2*x+1] = 128;
-    Y[2*y+1][2*x  ] = 128;
-    Y[2*y+1][2*x+1] = 128;
-    double u, v;
-    sincos(double(iter/8.0), &u, &v);
-    U[y][x] =-u * 127+128;
-    V[y][x] = v * 127+128;
+
+  // Compute color
+  Iter2YUV(iter, colorFullY[idxy][idxx], colorFullU[idxy][idxx], colorFullV[idxy][idxx]);
+  __syncthreads();
+
+  // Merge colors
+  if ( threadIdx.x % kPixelDim == 0 && threadIdx.y % kPixelDim == 0 ) {
+    int ytmp = 0;
+    for ( auto j = 0; j < kPixelDim; ++j ) {
+      for ( auto i = 0; i < kPixelDim; ++i ) {
+        ytmp += colorFullY[idxy+j][idxx+i];
+      }
+    }
+    colorY[idxy/kPixelDim][idxx/kPixelDim] = ytmp/(kPixelDim*kPixelDim);
+  }
+  if ( threadIdx.x == 0 && threadIdx.y == 0 ) {
+    int utmp = 0, vtmp = 0;
+    for ( auto j = 0; j < 2*kPixelDim; ++j ) {
+      for ( auto i = 0; i < 2*kPixelDim; ++i ) {
+        utmp += colorFullU[idxy+j][idxx+i];
+        vtmp += colorFullV[idxy+j][idxx+i];
+      }
+    }
+    colorU[blockIdx.y][blockIdx.x] = utmp/(4*kPixelDim*kPixelDim);
+    colorV[blockIdx.y][blockIdx.x] = vtmp/(4*kPixelDim*kPixelDim);
   }
 }
 
 struct Lab2VideoGenerator::Impl {
   int t = 0;
+  uint8_t *colorFullY, *colorFullU, *colorFullV;
 };
 
 Lab2VideoGenerator::Lab2VideoGenerator() : impl( new Impl ) {
+  cudaMalloc(&(impl->colorFullY), sizeof(uint8_t) * kFullW * kFullH);
+  cudaMalloc(&(impl->colorFullU), sizeof(uint8_t) * kFullW * kFullH);
+  cudaMalloc(&(impl->colorFullV), sizeof(uint8_t) * kFullW * kFullH);
 }
 
-Lab2VideoGenerator::~Lab2VideoGenerator() {}
+Lab2VideoGenerator::~Lab2VideoGenerator() {
+  cudaFree(impl->colorFullY);
+  cudaFree(impl->colorFullU);
+  cudaFree(impl->colorFullV);
+}
 
 void Lab2VideoGenerator::get_info( Lab2VideoInfo &info ) {
-  info.w = W;
-  info.h = H;
-  info.n_frame = NFRAME;
+  info.w = kW;
+  info.h = kH;
+  info.n_frame = kNFrame;
   info.fps_n = 24;
   info.fps_d = 1;
 };
 
 void Lab2VideoGenerator::Generate( uint8_t *yuv ) {
-  dim3 threadsPerBlock(16, 16);
-  dim3 numBlocks(W / 2 / threadsPerBlock.x, H / 2 / threadsPerBlock.y);
-  MandelbrotSet<<<numBlocks, threadsPerBlock>>>(reinterpret_cast<uint8_t (*)[W]>(yuv),
-                                                reinterpret_cast<uint8_t (*)[W/2]>(yuv+W*H),
-                                                reinterpret_cast<uint8_t (*)[W/2]>(yuv+W*H*5/4),
+  dim3 threadsPerBlock(2*kPixelDim, 2*kPixelDim);
+  dim3 numBlocks(kW/2, kH/2);
+  MandelbrotSet<<<numBlocks, threadsPerBlock>>>(reinterpret_cast<uint8_t (*)[kW]>(yuv),
+                                                reinterpret_cast<uint8_t (*)[kW/2]>(yuv+kW*kH),
+                                                reinterpret_cast<uint8_t (*)[kW/2]>(yuv+kW*kH*5/4),
+                                                reinterpret_cast<uint8_t (*)[kFullW]>(impl->colorFullY),
+                                                reinterpret_cast<uint8_t (*)[kFullW]>(impl->colorFullU),
+                                                reinterpret_cast<uint8_t (*)[kFullW]>(impl->colorFullV),
                                                 impl->t);
   ++(impl->t);
 }
