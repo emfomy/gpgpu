dadd5046c4c68dbe453a4be096b0b96d9ed69cde
Mu Yang <emfomy@gmail.com>
2016-05-03 15:07:37 +0800

    [lab3] Fix the race condition.

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -13,26 +13,25 @@ __global__ void PoissonImageCloningKernel(
   const float *mask,
   float *output,
   const int wb, const int hb, const int wt, const int ht,
-  const int oy, const int ox
+  const int oy, const int ox,
+  const bool status
 ) {
   const int yt = blockIdx.y * blockDim.y + threadIdx.y;
   const int xt = blockIdx.x * blockDim.x + threadIdx.x;
   const int curt = wt*yt+xt;
-  if ( 0 < yt && yt < ht-1 && 0 < xt && xt < wt-1 && mask[curt] > 127.0f ) {
+  if ( (yt % 2) ^ (xt % 2) == status && 0 < yt && yt < ht-1 && 0 < xt && xt < wt-1 && mask[curt] > 127.0f ) {
     const int yb = oy+yt, xb = ox+xt;
     const int curb = wb*yb+xb;
     if ( 0 < yb && yb < hb-1 && 0 < xb && xb < wb-1 ) {
-      for ( i = 0; i < 20000; ++i ) {
-        output[curb*3+0] = target[curt*3+0]
-                         +(output[(curb-wb)*3+0] + output[(curb-1)*3+0] + output[(curb+1)*3+0] + output[(curb+wb)*3+0]
-                         - target[(curt-wt)*3+0] - target[(curt-1)*3+0] - target[(curt+1)*3+0] - target[(curt+wt)*3+0]) / 4;
-        output[curb*3+1] = target[curt*3+1]
-                         +(output[(curb-wb)*3+1] + output[(curb-1)*3+1] + output[(curb+1)*3+1] + output[(curb+wb)*3+1]
-                         - target[(curt-wt)*3+1] - target[(curt-1)*3+1] - target[(curt+1)*3+1] - target[(curt+wt)*3+1]) / 4;
-        output[curb*3+2] = target[curt*3+2]
-                         +(output[(curb-wb)*3+2] + output[(curb-1)*3+2] + output[(curb+1)*3+2] + output[(curb+wb)*3+2]
-                         - target[(curt-wt)*3+2] - target[(curt-1)*3+2] - target[(curt+1)*3+2] - target[(curt+wt)*3+2]) / 4;
-      }
+      output[curb*3+0] = target[curt*3+0]
+                       +(output[(curb-wb)*3+0] + output[(curb-1)*3+0] + output[(curb+1)*3+0] + output[(curb+wb)*3+0]
+                       - target[(curt-wt)*3+0] - target[(curt-1)*3+0] - target[(curt+1)*3+0] - target[(curt+wt)*3+0]) / 4;
+      output[curb*3+1] = target[curt*3+1]
+                       +(output[(curb-wb)*3+1] + output[(curb-1)*3+1] + output[(curb+1)*3+1] + output[(curb+wb)*3+1]
+                       - target[(curt-wt)*3+1] - target[(curt-1)*3+1] - target[(curt+1)*3+1] - target[(curt+wt)*3+1]) / 4;
+      output[curb*3+2] = target[curt*3+2]
+                       +(output[(curb-wb)*3+2] + output[(curb-1)*3+2] + output[(curb+1)*3+2] + output[(curb+wb)*3+2]
+                       - target[(curt-wt)*3+2] - target[(curt-1)*3+2] - target[(curt+1)*3+2] - target[(curt+wt)*3+2]) / 4;
     }
   }
 }
@@ -49,8 +48,14 @@ void PoissonImageCloning(
   const int oy, const int ox
 ) {
   cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
-  PoissonImageCloningKernel<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
-    background, target, mask, output,
-    wb, hb, wt, ht, oy, ox
-  );
+  for ( auto i = 0; i < 20000; ++i ) {
+    PoissonImageCloningKernel<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
+      background, target, mask, output,
+      wb, hb, wt, ht, oy, ox, true
+    );
+    PoissonImageCloningKernel<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
+      background, target, mask, output,
+      wb, hb, wt, ht, oy, ox, false
+    );
+  }
 }
